a1f94634cbcb94e5a6572e2c58b190c0454a3852
Dominduchami <dominikkobinski314@gmail.com>
2024-12-23 14:21:01 +0100

    halextvic: bugcheck on entrypoint

--- halextmsmvic/halextapi.c
+++ halextmsmvic/halextapi.c
@@ -373,7 +373,7 @@ NTSTATUS HalExtensionInit(
 	__out POS_EXTENSION_HAL_EXTENSION_EXPORTS* Exports,
 	__in POS_EXTENSION_HAL_EXTENSION_IMPORTS Imports
 ) {
-	KeBugCheckEx(0x5D, 0x31415927, 0, 0, 0); //check if we reach init
+	KeBugCheckEx(0x5E, 0x31415927, 0, 0, 0); //check if we reach init
 	if (Interface != OsExtensionHalExtension) return STATUS_NOINTERFACE;
 	*Exports = &s_Exports;
 	s_Imports = Imports;
